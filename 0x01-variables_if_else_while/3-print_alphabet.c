d3daf691473963f057f9a7529b4eb0dd9116dfee
Saeed-Ali-1 <saidalisaid292@gmail.com>
2023-10-09 12:46:27 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * description: print all the letters
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char c = 'A';
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	while (C <= 'Z')
+	{
+	putchar(C);
+	C++;
+	}
+	putchar('\n');
+	return (0);
+}
