48a8345c13bd664ea4b2c7583c8cbe85ca6f6cf8
Saeed-Ali-1 <saidalisaid292@gmail.com>
2023-10-09 13:25:49 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * description: print the lowercase letters
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'z')
+	{
+	putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0);
+}
