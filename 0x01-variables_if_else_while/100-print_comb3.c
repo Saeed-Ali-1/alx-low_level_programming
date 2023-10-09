4f9461e308749787547da6d694991318fbf39914
Saeed-Ali-1 <saidalisaid292@gmail.com>
2023-10-09 15:18:53 -0700

    this is a commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (int i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (int j = i + 1; j <= 9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+		putchar(i + '0');
+		putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
