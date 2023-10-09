da5811433f487d06f6ceb96e0f68a3583328dbae
Saeed-Ali-1 <saidalisaid292@gmail.com>
2023-10-09 13:16:32 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * description: print numbers from 0 to 10
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
