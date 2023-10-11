fe722e941c27af62a067b210a5580d58e540f380
Saeed-Ali-1 <saidalisaid292@gmail.com>
2023-10-10 17:07:23 -0700

    this is a commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * description: positive or negative
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+	return (0);
+}
